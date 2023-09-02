3c17490cc7fcb4dd304267bf99d4610b7043ea24
forMedusa <amaan5054@gmail.com>
2023-09-02 16:45:50 +0530

    Updated the mongodb url

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,16 @@ const app= express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyparser.json());
+const userName = encodeURIComponent("Xyzsor");
+const pass = encodeURIComponent("Smackthat@369");
+console.log(userName)
+console.log(pass)
 // app.use(function(res,req,next){
 //     res.headers('Access-Control-Allow-Origin','*');
 //     res.headers('Access-Control-Allow-Headers','Orgin, X-Requested-With,Content-Type,Accept');
 //     next();
 // })
-const url = `mongodb://0.0.0.0:27017/notetakingapp`;
+const url = `mongodb+srv://${userName}:${pass}@cluster0.dm362or.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(url);
 client.connect();
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }); // Add options

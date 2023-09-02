54b94ab351a430b94069bd1da6fd9f20a177d216
forMedusa <amaan5054@gmail.com>
2023-09-03 00:46:10 +0530

    Latest Update

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(bodyparser.json());
 const userName = encodeURIComponent("Xyzsor");
 const pass = encodeURIComponent("Smackthat@369");
 app.use(function(req,res,next){
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://main--venerable-dodol-d18016.netlify.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 })
